779054ab57c6aa4e75461f9d420ba205215d2a50
hyunjjang <qqqq_o@naver.com>
2020-06-04 00:46:18 +0900

    sdsdasdas

--- Project1/Project1/q5.c
+++ Project1/Project1/q5.c
@@ -7,7 +7,7 @@ int main(void) {
 	//int odd_count=0;
 	printf("정수를 빈칸으로 구분해서 입력하세요.(마지막에 0 입력)\n");
 	while (num != 0) {
-		scanf("%d", &num);
+		scanf(" %d", &num);
 		
 		if (num == 0) {
 			printf("%d", num);
