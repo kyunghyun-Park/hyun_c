415d874d566b1a3b80e9d29b7434c5f03c4f7a75
kyunghyun-park <qqqq_o@naver.com>
2020-06-15 16:07:55 +0900

    zzzzzzzzzz

--- Project2/main.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-	printf("깃 연동 확인");
-}
---
+++ 변정민t_report(포인터)/p1.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int gong, num1;
+	int arr[10] = { 0 };
+	int* parr = arr;
+
+	printf("첫 번째 항? ");
+	scanf("%d", &num1);
+	printf("공차? ");
+	scanf("%d", &gong);
+
+	arr[0] = num1;
+
+	arith_seq(&arr, 10, gong);
+
+	printf("등차수열: ");
+	for (int i = 0; i < 10; i++) {
+		printf(" %3d", arr[i]);
+	}
+
+}
+
+int arith_seq(int *parr, int size, int com) {
+	int a = 0;
+	for (int i = 1; i <size; i++) {
+		parr[i] = parr[i-1] + 2;
+
+	}
+	return parr;
+}
