2b4202c4f7fb46309c66fb63442ce55172921239
kyunghyun-park <qqqq_o@naver.com>
2020-06-18 14:14:40 +0900

    asdsada

---
+++ 변정민t_report/변정민t_report(문자열)/q1.c
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{
+	int count = 0;
+	char str[] = "Certain words in a C program special meaning, they are keywords.";
+	char* pstr = str;
+
+	puts(str);
+
+	
+	while (pstr[0] != '\0') {
+		if (isspace(pstr[0]))
+			count++;
+		pstr++;
+	}
+	printf("공백 문자의 개수: %d\n",count);
+
+}
